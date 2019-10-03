3791f17a6c92f789c4aa6b1ef5ee569aced9919d
Debora <debora.v@hotmail.fr>
2019-10-03 21:35:21 +0200

    petit changement de derniere minute

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,41 +1,27 @@
-def throw_dice
+floor_count = 0
+
+until floor_count == 10
     dice = rand(1..6)
     puts "Lance le dé : #{dice}"
-end
 
-def game(dice)
-    floor_count = 0
-
-    #while floor_count < 10
-    
     if dice == 5 || dice == 6
         puts "Tu montes d'une marche."
+        floor_count += 1
         puts "Tu es à la marche suivante : #{floor_count}"
-        puts "Lance à nouveau le dé : #{dice}"
-        floor_count += 0
     
     elsif dice == 1 
         if floor_count == 0
             puts "Tu ne vas quand même pas aller plus bas !"
+        else
+            puts "Oh non !! Tu descends d'une marche."
+            floor_count -= 1
+            puts "Tu es à la marche precedente : #{floor_count}"
         end
-        puts "Oh non !! Tu descends d'une marche."
-        puts "Tu es à la marche suivante :#{floor_count}"
-        puts "Lance à nouveau le dé : #{dice}"
-        floor_count -= 2
     
     else
         puts "Malheureusement tu ne bouges pas."
-        puts "Tu es à la marche suivante : #{floor_count}"
-        puts "Lance à nouveau le dé : #{dice}"
-        floor_count -= 1
+        puts "Tu es à la marche : #{floor_count}"
     end
-    floor_count += 1
-end
-#end
-
-def perform
-    dice = throw_dice
-    game(dice)
 end
 
-perform
+puts "Bravo tu as fait 10 ! le jeu est terminé"
